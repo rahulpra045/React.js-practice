e5188bf567a0c6d8c8252e3bf8fd88a85fffc6d2
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2024-07-28 11:52:07 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import HelloWorld from './HelloWorld'
+import Counter from './Counter'
 export default function App () {
   return (
     <div>
       <HelloWorld />
+      <Counter />
     </div>
   )
 }
