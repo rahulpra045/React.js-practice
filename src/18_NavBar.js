4613c9c9010b81d0d3fef8ddfbf134dfd402d622
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2024-08-07 19:42:42 +0530

    navbar-responsive-hamburger

--- src/18_NavBar.js
+++ src/18_NavBar.js
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './styles/18_NavBar.css'
+import { CiMenuFries } from "react-icons/ci";
 const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
+
   return (
-    <div>NavBar</div>
+      <div className="navbar">
+          <button className='menu-icon' onClick={toggleMenu}>
+              <CiMenuFries/>
+          </button>
+          <ul className={`menu ${isOpen ? 'open':''}`}>
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contacts</li>
+          </ul>
+    </div>
   )
 }
 
